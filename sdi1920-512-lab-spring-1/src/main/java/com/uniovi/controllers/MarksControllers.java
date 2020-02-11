51e0a11b073420f0cd05406cf17e759bb2d3b5bf
nuriasanemeterio <nuria.sane.r@gmail.com>
2020-02-11 10:11:30 +0100

    “SDI - 1.8 Trabajando con Controladores.”

--- sdi1920-512-lab-spring-1/src/main/java/com/uniovi/controllers/MarksControllers.java
+++ sdi1920-512-lab-spring-1/src/main/java/com/uniovi/controllers/MarksControllers.java
@@ -1,8 +1,13 @@
 package com.uniovi.controllers;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.uniovi.entities.Mark;
 
 @RestController
 public class MarksControllers {
@@ -11,13 +16,22 @@ public class MarksControllers {
 	public String getList(){
 	return "Getting List";
 	}
-	@RequestMapping("/mark/add")
+	@RequestMapping(value="/mark/add", method=RequestMethod.POST)
 	 public String setMark(){
 	return "Adding Mark";
 	}
-	@RequestMapping("/mark/details/{id} ")
-	public String getDetail(@RequestParam Long id){
-	return "Getting Details" +id;
+	
+	@RequestMapping(value="/mark/add", method=RequestMethod.POST)
+	public String setMark(@ModelAttribute Mark mark){
+		return "added: " + mark.getDescription()
+		+" with score : "+ mark.getScore()
+		+" id: " + mark.getId();
+
+	}
+	
+	@RequestMapping("/mark/details/{id}" )
+	public String getDetail(@PathVariable Long id){
+	return " Getting Detail: "+id;
 	}
 
 }
